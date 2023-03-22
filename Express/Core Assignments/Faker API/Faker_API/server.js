e0ce28771a08b082018d7d22ba671470b4e2a1f6
Aaron Hung <aahung00@gmail.com>
2023-03-21 21:05:44 -0700

    first commit "Core Assignment: Faker API"

---
+++ Express/Core Assignments/Faker API/Faker_API/server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+const { faker } = require("@faker-js/faker");
+
+const createUser = () => ({
+    _id: faker.datatype.uuid(),
+    firtName: faker.name.firstName(),
+    lastName: faker.name.lastName(),
+    phoneNumber: faker.phone.number(),
+    email: faker.internet.email(),
+    password: faker.internet.password()
+});
+
+const createCompany = () => ({
+    _id: faker.datatype.uuid(),
+    name: faker.company.name(),
+    address: {
+        street: faker.address.street(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+        zipCode: faker.address.zipCode(),
+        country: faker.address.country()
+    }
+});
+
+//  "/api/users/new"
+app.get("/api/users/new", (req, res) => {
+    res.json(createUser());
+});
+
+//  "/api/companies/new"
+app.get("/api/companies/new", (req, res) => {
+    res.json(createCompany());
+});
+
+//  "/api/user/company"
+app.get("/api/user/company", (req, res) => {
+    const newObject = {
+        user: createUser(),
+        company: createCompany()
+    };
+    res.json(newObject);
+});
+
+app.listen(port, () => console.log(`Listening on port: ${port}`));
