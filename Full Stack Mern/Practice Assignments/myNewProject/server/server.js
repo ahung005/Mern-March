f965ec1dc920ec1f87266a3b1d7dde608407069a
Aaron Hung <aahung00@gmail.com>
2023-03-28 22:45:18 -0700

    second commit "created back-end"

--- Full Stack Mern/Practice Assignments/myNewProject/server/server.js
+++ Full Stack Mern/Practice Assignments/myNewProject/server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors')    /* This is new */
 const app = express();
-app.use(cors())                 /* This is new */
 
 const port = 8000;
-    
+
+app.use(cors())                 /* This is new */
+app.use(express.json());                           /* This is new and allows JSON Objects to be posted */
+app.use(express.urlencoded({ extended: true }));   /* This is new and allows JSON Objects with strings and arrays*/
+
+require('./config/mongoose.config');    /* This is new */
 require('./routes/person.routes')(app);   // We're importing the routes export. 
 
 // These two lines are the long-hand notation of the code above. They better show what's going on.
